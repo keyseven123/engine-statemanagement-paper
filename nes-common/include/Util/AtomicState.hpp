nebulastream/nebulastream

refactor(QueryManager) Adds query engine implementation according to DD

nes-common/include/Util/AtomicState.hpp
@@ -1,0 +1,66 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <concepts>
+#include <functional>
+#include <mutex>
+#include <type_traits>
+#include <utility>
+#include <variant>
+
+/// This is a thread-safe variant like data structure, that allows atomic transition from one state into the other state.
+/// e.g. AtomicState<A,B> would allow a transition from A -> B by applying a function `B map(A&&)`
+template <typename... States>
+struct AtomicState
+{
+    /// Construct AtomicState from any valid State
+    template <typename T>
+    requires(std::same_as<std::remove_cvref_t<T>, States> || ...)
+    explicit AtomicState(T&& state) : state(std::forward<T>(state))
+    {
+    }
+
+    /// Attempts all transitions in order and stops at the first successful transition. If no transition was successful this function
+    /// returns false.
+    template <typename... TransitionFunctions>
+    bool transition(const TransitionFunctions&... transitionFunctions)
+    {
+        std::scoped_lock lock(mutex);
+        return (false || ... || try_transition_locked(std::function(transitionFunctions)));
+    }
+
+    /// Attempts a single transition. If the transition was successful this function returns true.
+    template <typename FromState, typename ToState>
+    bool transition(const std::function<ToState(FromState&&)>& transitionFunction)
+    {
+        std::scoped_lock lock(mutex);
+        return try_transition_locked(transitionFunction);
+    }
+
+private:
+    template <typename FromState, typename ToState>
+    bool try_transition_locked(const std::function<ToState(FromState&&)>& transitionFunction)
+    {
+        if (!std::holds_alternative<FromState>(state))
+        {
+            return false;
+        }
+        state = transitionFunction(std::move(std::get<FromState>(state)));
+        return true;
+    }
+    std::recursive_mutex mutex;
+    std::variant<States...> state;
+};

nes-query-engine/RunningSource.cpp
@@ -1,0 +1,118 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <functional>
+#include <memory>
+#include <utility>
+#include <variant>
+#include <vector>
+#include <Identifiers/Identifiers.hpp>
+#include <Sources/SourceReturnType.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <Interfaces.hpp>
+#include <RunningQueryPlan.hpp>
+#include <RunningSource.hpp>
+
+namespace NES::Runtime
+{
+
+template <typename... Ts>
+struct Overloaded : Ts...
+{
+    using Ts::operator()...;
+};
+
+namespace
+{
+Sources::SourceReturnType::EmitFunction emitFunction(
+    QueryId queryId,
+    std::weak_ptr<RunningSource> source,
+    std::vector<std::shared_ptr<RunningQueryPlanNode>> successors,
+    QueryLifetimeController& controller,
+    WorkEmitter& emitter)
+{
+    return [&controller, successors = std::move(successors), source, &emitter, queryId](
+               OriginId sourceId, Sources::SourceReturnType::SourceReturnType event)
+    {
+        std::visit(
+            Overloaded{
+                [&](Sources::SourceReturnType::Data data)
+                {
+                    for (const auto& successor : successors)
+                    {
+                        emitter.emitWork(queryId, successor, std::move(data.buffer), {}, {});
+                    }
+                },
+                [&](Sources::SourceReturnType::EoS) { controller.initializeSourceStop(queryId, sourceId, source); },
+                [&](Sources::SourceReturnType::Error error)
+                { controller.initializeSourceFailure(queryId, sourceId, source, std::move(error.ex)); }},
+            std::move(event));
+    };
+}
+}
+
+OriginId RunningSource::getOriginId() const
+{
+    return source->getSourceId();
+}
+RunningSource::RunningSource(
+    std::vector<std::shared_ptr<RunningQueryPlanNode>> successors,
+    std::unique_ptr<Sources::SourceHandle> pSource,
+    std::function<void()> unregister,
+    std::function<void(Exception)> unregisterWithError)
+    : successors(std::move(successors))
+    , source(std::move(pSource))
+    , unregister(std::move(unregister))
+    , unregisterWithError(std::move(unregisterWithError))
+{
+}
+
+std::shared_ptr<RunningSource> RunningSource::create(
+    QueryId queryId,
+    std::unique_ptr<Sources::SourceHandle> source,
+    std::vector<std::shared_ptr<RunningQueryPlanNode>> successors,
+    std::function<void()> unregister,
+    std::function<void(Exception)> unregisterWithError,
+    QueryLifetimeController& controller,
+    WorkEmitter& emitter)
+{
+    auto runningSource = std::shared_ptr<RunningSource>(
+        new RunningSource(successors, std::move(source), std::move(unregister), std::move(unregisterWithError)));
+    NES_DEBUG("Starting Running Source");
+    runningSource->source->start(emitFunction(queryId, runningSource, std::move(successors), controller, emitter));
+    return runningSource;
+}
+
+RunningSource::~RunningSource()
+{
+    if (source)
+    {
+        NES_DEBUG("Stopping Running Source");
+        if (!source->stop())
+        {
+            /// Currently we do not have to recover from a source which could not be properly closed as all our queries are one-shot.
+            NES_WARNING("Could not gracefully stop source");
+        }
+    }
+}
+void RunningSource::stop() const
+{
+    unregister();
+}
+void RunningSource::fail(Exception exception) const
+{
+    unregisterWithError(std::move(exception));
+}
+
+}

keyseven123/engine-statemanagement-paper

nes-common/include/Util/AtomicState.hpp
@@ -1,0 +1,66 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <concepts>
+#include <functional>
+#include <mutex>
+#include <type_traits>
+#include <utility>
+#include <variant>
+
+/// This is a thread-safe variant like data structure, that allows atomic transition from one state into the other state.
+/// e.g. AtomicState<A,B> would allow a transition from A -> B by applying a function `B map(A&&)`
+template <typename... States>
+struct AtomicState
+{
+    /// Construct AtomicState from any valid State
+    template <typename T>
+    requires(std::same_as<std::remove_cvref_t<T>, States> || ...)
+    explicit AtomicState(T&& state) : state(std::forward<T>(state))
+    {
+    }
+
+    /// Attempts all transitions in order and stops at the first successful transition. If no transition was successful this function
+    /// returns false.
+    template <typename... TransitionFunctions>
+    bool transition(const TransitionFunctions&... transitionFunctions)
+    {
+        std::scoped_lock lock(mutex);
+        return (false || ... || tryTransitionLocked(std::function(transitionFunctions)));
+    }
+
+    /// Attempts a single transition. If the transition was successful this function returns true.
+    template <typename FromState, typename ToState>
+    bool transition(const std::function<ToState(FromState&&)>& transitionFunction)
+    {
+        std::scoped_lock lock(mutex);
+        return tryTransitionLocked(transitionFunction);
+    }
+
+private:
+    template <typename FromState, typename ToState>
+    bool tryTransitionLocked(const std::function<ToState(FromState&&)>& transitionFunction)
+    {
+        if (!std::holds_alternative<FromState>(state))
+        {
+            return false;
+        }
+        state = transitionFunction(std::move(std::get<FromState>(state)));
+        return true;
+    }
+    std::mutex mutex;
+    std::variant<States...> state;
+};

nes-query-engine/RunningSource.cpp
@@ -1,0 +1,118 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <functional>
+#include <memory>
+#include <utility>
+#include <variant>
+#include <vector>
+#include <Identifiers/Identifiers.hpp>
+#include <Sources/SourceReturnType.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <Interfaces.hpp>
+#include <RunningQueryPlan.hpp>
+#include <RunningSource.hpp>
+
+namespace NES::Runtime
+{
+
+template <typename... Ts>
+struct Overloaded : Ts...
+{
+    using Ts::operator()...;
+};
+
+namespace
+{
+Sources::SourceReturnType::EmitFunction emitFunction(
+    QueryId queryId,
+    std::weak_ptr<RunningSource> source,
+    std::vector<std::shared_ptr<RunningQueryPlanNode>> successors,
+    QueryLifetimeController& controller,
+    WorkEmitter& emitter)
+{
+    return [&controller, successors = std::move(successors), source, &emitter, queryId](
+               OriginId sourceId, Sources::SourceReturnType::SourceReturnType event)
+    {
+        std::visit(
+            Overloaded{
+                [&](Sources::SourceReturnType::Data data)
+                {
+                    for (const auto& successor : successors)
+                    {
+                        emitter.emitWork(queryId, successor, std::move(data.buffer), {}, {});
+                    }
+                },
+                [&](Sources::SourceReturnType::EoS) { controller.initializeSourceStop(queryId, sourceId, source); },
+                [&](Sources::SourceReturnType::Error error)
+                { controller.initializeSourceFailure(queryId, sourceId, source, std::move(error.ex)); }},
+            std::move(event));
+    };
+}
+}
+
+OriginId RunningSource::getOriginId() const
+{
+    return source->getSourceId();
+}
+RunningSource::RunningSource(
+    std::vector<std::shared_ptr<RunningQueryPlanNode>> successors,
+    std::unique_ptr<Sources::SourceHandle> source,
+    std::function<void()> unregister,
+    std::function<void(Exception)> unregisterWithError)
+    : successors(std::move(successors))
+    , source(std::move(source))
+    , unregister(std::move(unregister))
+    , unregisterWithError(std::move(unregisterWithError))
+{
+}
+
+std::shared_ptr<RunningSource> RunningSource::create(
+    QueryId queryId,
+    std::unique_ptr<Sources::SourceHandle> source,
+    std::vector<std::shared_ptr<RunningQueryPlanNode>> successors,
+    std::function<void()> unregister,
+    std::function<void(Exception)> unregisterWithError,
+    QueryLifetimeController& controller,
+    WorkEmitter& emitter)
+{
+    auto runningSource = std::shared_ptr<RunningSource>(
+        new RunningSource(successors, std::move(source), std::move(unregister), std::move(unregisterWithError)));
+    NES_DEBUG("Starting Running Source");
+    runningSource->source->start(emitFunction(queryId, runningSource, std::move(successors), controller, emitter));
+    return runningSource;
+}
+
+RunningSource::~RunningSource()
+{
+    if (source)
+    {
+        NES_DEBUG("Stopping Running Source");
+        if (!source->stop())
+        {
+            /// Currently we do not have to recover from a source which could not be properly closed as all our queries are one-shot.
+            NES_WARNING("Could not gracefully stop source");
+        }
+    }
+}
+void RunningSource::stop() const
+{
+    unregister();
+}
+void RunningSource::fail(Exception exception) const
+{
+    unregisterWithError(std::move(exception));
+}
+
+}