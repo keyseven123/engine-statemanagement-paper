nebulastream/nebulastream

refactor(Lowering) Removes sources and sinks from lowering

nes-execution/include/QueryCompiler/Operators/ExecutableOperator.hpp
@@ -13,7 +13,8 @@
 */
 #pragma once
 #include <Operators/AbstractOperators/Arity/UnaryOperator.hpp>
-#include <Runtime/Execution/ExecutablePipelineStage.hpp>
+#include <Runtime/Execution/OperatorHandler.hpp>
+#include <Executable.hpp>
 
 namespace NES::QueryCompilation
 {
@@ -40,7 +41,9 @@ public:
      * @brief Gets the executable pipeline stage.
      * @return ExecutablePipelineStagePtr
      */
-    Runtime::Execution::ExecutablePipelineStagePtr getExecutablePipelineStage();
+    const Runtime::Execution::ExecutablePipelineStage& getExecutablePipelineStage() const;
+
+    Runtime::Execution::ExecutablePipelineStagePtr takeExecutablePipelineStage();
 
     /**
      * @brief Gets the operator handlers, which capture specific operator state.

nes-execution/src/QueryCompiler/Operators/ExecutableOperator.cpp
@@ -11,9 +11,13 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+#include <memory>
 #include <utility>
+#include <Identifiers/Identifiers.hpp>
 #include <Operators/Operator.hpp>
 #include <QueryCompiler/Operators/ExecutableOperator.hpp>
+#include <ErrorHandling.hpp>
+#include <Executable.hpp>
 
 namespace NES::QueryCompilation
 {
@@ -37,9 +41,13 @@ OperatorPtr ExecutableOperator::create(
         ExecutableOperator(getNextOperatorId(), std::move(executablePipelineStage), std::move(operatorHandlers)));
 }
 
-Runtime::Execution::ExecutablePipelineStagePtr ExecutableOperator::getExecutablePipelineStage()
+const Runtime::Execution::ExecutablePipelineStage& ExecutableOperator::getExecutablePipelineStage() const
 {
-    return executablePipelineStage;
+    return *executablePipelineStage;
+}
+std::unique_ptr<Runtime::Execution::ExecutablePipelineStage> ExecutableOperator::takeExecutablePipelineStage()
+{
+    return std::move(executablePipelineStage);
 }
 
 std::vector<Runtime::Execution::OperatorHandlerPtr> ExecutableOperator::getOperatorHandlers()
@@ -54,7 +62,7 @@ std::string ExecutableOperator::toString() const
 
 OperatorPtr ExecutableOperator::copy()
 {
-    return create(executablePipelineStage, operatorHandlers);
+    throw NotImplemented();
 }
 
 }

keyseven123/engine-statemanagement-paper

nes-execution/include/QueryCompiler/Operators/ExecutableOperator.hpp
@@ -13,7 +13,8 @@
 */
 #pragma once
 #include <Operators/AbstractOperators/Arity/UnaryOperator.hpp>
-#include <Runtime/Execution/ExecutablePipelineStage.hpp>
+#include <Runtime/Execution/OperatorHandler.hpp>
+#include <Executable.hpp>
 
 namespace NES::QueryCompilation
 {
@@ -40,7 +41,9 @@ public:
      * @brief Gets the executable pipeline stage.
      * @return ExecutablePipelineStagePtr
      */
-    Runtime::Execution::ExecutablePipelineStagePtr getExecutablePipelineStage();
+    const Runtime::Execution::ExecutablePipelineStage& getStage() const;
+
+    Runtime::Execution::ExecutablePipelineStagePtr takeStage();
 
     /**
      * @brief Gets the operator handlers, which capture specific operator state.

nes-execution/src/QueryCompiler/Operators/ExecutableOperator.cpp
@@ -11,9 +11,13 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+#include <memory>
 #include <utility>
+#include <Identifiers/Identifiers.hpp>
 #include <Operators/Operator.hpp>
 #include <QueryCompiler/Operators/ExecutableOperator.hpp>
+#include <ErrorHandling.hpp>
+#include <Executable.hpp>
 
 namespace NES::QueryCompilation
 {
@@ -37,9 +41,13 @@ OperatorPtr ExecutableOperator::create(
         ExecutableOperator(getNextOperatorId(), std::move(executablePipelineStage), std::move(operatorHandlers)));
 }
 
-Runtime::Execution::ExecutablePipelineStagePtr ExecutableOperator::getExecutablePipelineStage()
+const Runtime::Execution::ExecutablePipelineStage& ExecutableOperator::getStage() const
 {
-    return executablePipelineStage;
+    return *executablePipelineStage;
+}
+std::unique_ptr<Runtime::Execution::ExecutablePipelineStage> ExecutableOperator::takeStage()
+{
+    return std::move(executablePipelineStage);
 }
 
 std::vector<Runtime::Execution::OperatorHandlerPtr> ExecutableOperator::getOperatorHandlers()
@@ -54,7 +62,7 @@ std::string ExecutableOperator::toString() const
 
 OperatorPtr ExecutableOperator::copy()
 {
-    return create(executablePipelineStage, operatorHandlers);
+    throw NotImplemented();
 }
 
 }