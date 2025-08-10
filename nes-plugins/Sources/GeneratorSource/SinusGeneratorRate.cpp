8f7746dab1aa8ab6065c720b1818aab3a433a53a
Nils L. Schubert <nilslpschubert@gmail.com>
2025-08-10 19:58:13 +0200

    feat(Generator Source): Added output rates to generator source
    It is now possible to specify a type of output / emit rate. For now, we have implemented a fixed and a sinus rate.

---
+++ nes-plugins/Sources/GeneratorSource/SinusGeneratorRate.cpp
@@ -0,0 +1,102 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <fmt/format.h>
+#include <ErrorHandling.hpp>
+#include <SinusGeneratorRate.hpp>
+
+namespace NES::Sources
+{
+
+SinusGeneratorRate::SinusGeneratorRate(const std::string_view configString)
+{
+    /// We can assume that the parsing will work, as the config has been validated
+    auto [amplitude, frequency, phaseShift] = parseAndValidateConfigString(configString).value();
+    this->amplitude = amplitude;
+    this->frequency = frequency;
+    this->phaseShift = phaseShift;
+}
+
+std::optional<std::tuple<double, double, double>> SinusGeneratorRate::parseAndValidateConfigString(std::string_view configString)
+{
+    std::optional<double> amplitude = {};
+    std::optional<double> frequency = {};
+    std::optional<double> phaseShift = {};
+    for (const auto& line : configString | std::views::split('\n'))
+    {
+        auto lineView{std::string_view(line.begin(), line.end())};
+        const auto spacePos = lineView.find(' ');
+        if (spacePos != std::string_view::npos)
+        {
+            auto key = lineView.substr(0, spacePos);
+            auto value = lineView.substr(spacePos + 1);
+
+            std::string keyLower;
+            std::ranges::transform(key, std::back_inserter(keyLower), [](const unsigned char c) { return std::tolower(c); });
+
+            if (keyLower == "amplitude")
+            {
+                amplitude = std::stod(std::string(value));
+            }
+            else if (keyLower == "frequency")
+            {
+                frequency = std::stod(std::string(value));
+            }
+            else if (keyLower == "phase_shift")
+            {
+                phaseShift = std::stod(std::string(value));
+            }
+        }
+    }
+
+
+    if (amplitude.has_value() and frequency.has_value() and phaseShift.has_value())
+    {
+        return std::make_tuple(amplitude.value(), frequency.value(), phaseShift.value());
+    }
+    return {};
+}
+
+uint64_t SinusGeneratorRate::calcNumberOfTuplesForInterval(
+    const std::chrono::time_point<std::chrono::system_clock>& start, const std::chrono::time_point<std::chrono::system_clock>& end)
+{
+    /// To calculate the number of tuples for a non-negative sinus, we take the integral of the sinus from end to start
+    /// As the sinus must be non-negative, the sin-function is emit_rate_at_x = amplitude * |sin(freq * (x - phaseShift))|
+    /// Thus, the integral of the function is: - (amplitude * sin(2 * freq * (x - phaseShift))) / (2 * freq * |sin(freq * (x - phaseShift))|)
+    auto integral = [](const double timepoint, const double amplitude, const double frequency, const double phaseShift)
+    {
+        const auto dividend = amplitude * std::sin(2 * frequency * (timepoint - phaseShift));
+        const auto divisor = 2 * frequency * std::abs(std::sin(frequency * (timepoint - phaseShift)));
+        return -1.0 * dividend / divisor;
+    };
+
+    /// Calculating the integral of end --> start, resulting in the required number of tuples
+    /// As the interval of start and end might share the same seconds, we need to first cast it to milliseconds and then convert it to a
+    /// double. Otherwise, we would have the exact same value for startTimePoint and endTimePoint, resulting in no tuples to generate
+    const auto startTimePoint = std::chrono::duration<double>(start.time_since_epoch()).count();
+    const auto endTimePoint = std::chrono::duration<double>(end.time_since_epoch()).count();
+    const auto startIntegralValue = integral(startTimePoint, amplitude, frequency, phaseShift);
+    const auto endIntegralValue = integral(endTimePoint, amplitude, frequency, phaseShift);
+    const auto numberOfTuples = endIntegralValue - startIntegralValue;
+    return numberOfTuples;
+}
+
+}
