ea09ef2fdc7156aeb461801a6964bd8c7889da66
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-08-24 15:12:45 +0200

    refactor(engine): add task completion callback
    There were issues where the reference reducer was not
    called as it was part of the (now called) success callback
    which was only called if the task returned true.
    
    This commit adds onComplete callback which is called
    regardless. It uses absl::AnyInvocable for its enforced
    move semantics which make it impossible for a callback
    to be executed multiple times.

--- nes-query-engine/tests/Util/QueryEngineTestingInfrastructure.hpp
+++ nes-query-engine/tests/Util/QueryEngineTestingInfrastructure.hpp
@@ -162,25 +162,11 @@ struct TestWorkEmitter : WorkEmitter
     MOCK_METHOD(
         bool,
         emitWork,
-        (QueryId,
-         const std::shared_ptr<RunningQueryPlanNode>&,
-         Memory::TupleBuffer,
-         BaseTask::onComplete,
-         BaseTask::onFailure,
-         PipelineExecutionContext::ContinuationPolicy),
+        (QueryId, const std::shared_ptr<RunningQueryPlanNode>&, Memory::TupleBuffer, TaskCallback, PipelineExecutionContext::ContinuationPolicy),
         (override));
-    MOCK_METHOD(
-        void,
-        emitPipelineStart,
-        (QueryId, const std::shared_ptr<RunningQueryPlanNode>&, BaseTask::onComplete, BaseTask::onFailure),
-        (override));
-    MOCK_METHOD(
-        void,
-        emitPendingPipelineStop,
-        (QueryId, std::shared_ptr<RunningQueryPlanNode>, BaseTask::onComplete, BaseTask::onFailure),
-        (override));
-    MOCK_METHOD(
-        void, emitPipelineStop, (QueryId, std::unique_ptr<RunningQueryPlanNode>, BaseTask::onComplete, BaseTask::onFailure), (override));
+    MOCK_METHOD(void, emitPipelineStart, (QueryId, const std::shared_ptr<RunningQueryPlanNode>&, TaskCallback), (override));
+    MOCK_METHOD(void, emitPendingPipelineStop, (QueryId, std::shared_ptr<RunningQueryPlanNode>, TaskCallback), (override));
+    MOCK_METHOD(void, emitPipelineStop, (QueryId, std::unique_ptr<RunningQueryPlanNode>, TaskCallback), (override));
 };
 
 struct TestQueryLifetimeController : QueryLifetimeController
